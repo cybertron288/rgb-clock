ddeca2664bf2229c39de9b5628b0ab911672af98
Ravi Patel <ravi60353@gmail.com>
2024-06-26 01:02:04 +0530

    chore: code cleanup and refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,58 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useMemo } from "react"
 import { motion } from "framer-motion"
 import useOscillatingValue from "./hooks/useOscillatingValue"
 import ColorService from "./service/ColorService"
 
 const App = () => {
-  const [hours, setHours] = useState(new Date().getHours())
-  const [minutes, setMinutes] = useState(new Date().getMinutes())
-  const [seconds, setSeconds] = useState(new Date().getSeconds())
-
-  const { getContrastingColor } = ColorService
+  const [time, setTime] = useState({
+    hours: new Date().getHours(),
+    minutes: new Date().getMinutes(),
+    seconds: new Date().getSeconds(),
+  })
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setHours(new Date().getHours())
-      setMinutes(new Date().getMinutes())
-      setSeconds(new Date().getSeconds())
+      setTime({
+        hours: new Date().getHours(),
+        minutes: new Date().getMinutes(),
+        seconds: new Date().getSeconds(),
+      })
     }, 1000)
     return () => clearInterval(interval)
   }, [])
 
-  const red = useOscillatingValue(hours, 23)
-  const green = useOscillatingValue(minutes, 59)
-  const blue = useOscillatingValue(seconds, 59)
+  const red = useOscillatingValue(time.hours, 23)
+  const green = useOscillatingValue(time.minutes, 59)
+  const blue = useOscillatingValue(time.seconds, 59)
 
   const backgroundColor = `rgb(${Math.floor(red)}, ${Math.floor(
     green
   )}, ${Math.floor(blue)})`
-  const hourRotation = (hours / 12) * 360
-  const minuteRotation = (minutes / 60) * 360
-  const secondRotation = (seconds / 60) * 360
 
-  return (
-    <>
-      <motion.div
-        className="w-screen h-screen flex items-center justify-center"
-        animate={{ backgroundColor }}
-        transition={{ duration: 1 }}
-      >
-        {/* <div className="flex space-x-4 relative z-10">
-          <Triangle rotation={hourRotation} color="red" />
-          <Triangle rotation={minuteRotation} color="green" />
-          <Triangle rotation={secondRotation} color="blue" />
-        </div> */}
-        <div className="h-full absolute flex flex-col justify-center z-[2] opacity-40">
-          <div className="kanit-bold text-[240px]">
-            {hours}:{minutes}:{seconds}
-          </div>
-        </div>
-        <div
-          className={`absolute bottom-4 right-4 text-right flex flex-col gap-1 z-[4] text-[${getContrastingColor(
-            red,
-            green,
-            blue
-          )}] z-2`}
-        >
-          <div className="kanit-medium text-[40px] leading-[40px]">
-            "RGB Clock"
-          </div>
-        </div>
-      </motion.div>
-    </>
+  const textColor = useMemo(
+    () => ColorService.getContrastingColor(red, green, blue),
+    [red, green, blue]
   )
-}
 
-const Triangle = ({ rotation, color }) => {
   return (
     <motion.div
-      className="w-60 h-60 flex items-center justify-center"
-      animate={{ rotate: rotation }}
-      transition={{ type: "spring", stiffness: 100 }}
+      className="w-screen h-screen flex items-center justify-center"
+      animate={{ backgroundColor }}
+      transition={{ duration: 1 }}
     >
+      <div className="h-full absolute flex flex-col justify-center z-[2] opacity-40">
+        <div className="kanit-bold text-[240px]">
+          {time.hours}:{time.minutes}:{time.seconds}
+        </div>
+      </div>
       <div
-        className="w-16 h-16 border-black border-2 border-b-60"
-        style={{ borderBottomColor: color }}
-      ></div>
+        className="absolute bottom-4 right-4 text-right flex flex-col gap-1 z-[4]"
+        style={{ color: textColor }}
+      >
+        <div className="kanit-medium text-[40px] leading-[40px]">
+          "RGB Clock"
+        </div>
+      </div>
     </motion.div>
   )
 }
